7f4f4a4f428ba1884ec946dd98ad5d834a988c93
Zev Landau <zev@landausoftware.com>
2019-12-02 22:55:48 -0500

    Activated service workder with on update function

--- src/index.js
+++ src/index.js
@@ -9,4 +9,17 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register({
+  onUpdate: reg => {
+    const waiting = reg.waiting
+
+    if (waiting) {
+      waiting.addEventListener('statechange', event => {
+        if (event.target.state === 'activated') {
+          window.location.reload()
+        }
+      })
+      waiting.postMessage({type: 'SKIP_WAITING'})
+    }
+  }
+});
